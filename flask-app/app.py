cc730544c7af071456f59239efd88d7331f12c09
dhathriv25 <dhathri.dha3@gmail.com>
2025-03-03 11:58:36 +0000

    Update app.py
    Rolling back to port 80

--- flask-app/app.py
+++ flask-app/app.py
@@ -7,5 +7,5 @@ def home():
     return '<h1>WELL DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!!!!!!!!!!!!!!!!!!!!!!\n\n NEWWW \n\nUsing Port 8080</h1>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
 
