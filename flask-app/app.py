93f036dcfc903b53af7e0ef765827e4da63a11c3
anishpimple <31899357+anishpimple@users.noreply.github.com>
2025-03-03 01:27:05 +0000

    Update app.py
    Attempt 2

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Let's F****** GO!!</h1>'
+    return '<h1>Hey the CI/CD is working </h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
